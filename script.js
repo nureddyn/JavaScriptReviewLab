ec0698b4b29f5088ccb3d4683bc20297cdc8e9c0
nureddyn <nureddynballout@gmail.com>
2023-10-24 15:16:03 -0300

    II part done

--- script.js
+++ script.js
@@ -93,3 +93,63 @@ a message should print "Sorry, you're too young."*/
 if (personAge >= 16) console.log("Here are the keys!")
 else console.log("Sorry, you're too young.");
 
+// II. Loops
+// A. The basics
+// 1. Write a loop that will print out all the numbers from 0 to 10, inclusive
+for (let i = 0; i <= 10; i++) console.log(i);
+
+// 2. Write a loop that will print out all the numbers from 10 up to and including 400
+for (let i = 0; i <= 400; i++) console.log(i);
+
+// 3. Write a loop that will print out every third number starting with 12 and going no higher than 4000
+for (let i = 12; i <= 4000; i+=3) console.log(i);
+
+// B. Get even
+// 1. Print out the numbers that are within the range of 1 - 100
+//for (let i = 1; i <= 100; i++) console.log(i);
+
+// 2. Adjust your code to add a message next to even numbers only that says: "<-- is an even number"
+for (let i = 1; i <= 100; i++) {
+    if (i % 2 === 0) console.log(`${i} <-- is an even number`);
+    else console.log(i);
+}
+
+// C. Give me Five
+// 1. For the numbers 0 - 100, print out "I found a number. High five!" if the number is a multiple of five 
+for (let number = 0; number <=100; number++) {
+    if (number % 5 === 0) console.log(`I found a ${number}. High five!`);
+}
+
+// 2. Add to the code from above to print out "I found a number. Three is a crowd" if the number is a multiple of three
+for (let number = 0; number <=100; number++) {
+    if (number % 5 === 0) console.log(`I found a ${number}. High five!`);
+    else if (number % 3 === 0) console.log(`I found a ${number}. Three is a crowd`);
+}
+
+
+// 3. For numbers divisible by both three and five, be sure your code prints both messages
+for (let number = 0; number <=100; number++) {
+    if (number % 5 === 0) console.log(`I found a ${number}. High five!`);
+    else if (number % 3 === 0) console.log(`I found a ${number}. Three is a crowd`);
+    else if (number % 3 === 0 && number % 5 === 0) {
+        console.log(`I found a ${number}. Three is a crowd`);
+        console.log(`I found a ${number}. High five!`);
+    }
+}
+
+// D. Saving account
+// 1. Write code that will save the sum of all the numbers between 1 - 10 to a variable called bank_account
+let bank_account = 0;
+for (let n = 1; n <= 10; n++) bank_account+=n;
+
+// Check your work! Your bank_account should have $55 in it.
+console.log(bank_account);
+
+/* 3. You got a bonus! Your pay is now doubled each week. Write code that will save the sum of all the numbers between 1 - 100
+multiplied by 2.*/
+bank_account = 0;
+for (let n = 1; n <= 100; n++) bank_account+=n*2;
+
+// Check your work! Your bank_account should have $10,100 in it.
+console.log(bank_account);
+
